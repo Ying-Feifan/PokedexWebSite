8da93224e9572fb7443a77803045a2d26b497ff5
Ying-Feifan <ying_byl@hotmail.com>
2020-05-14 12:25:10 -0300

    delete main component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { MainComponent } from './main/main.component';
+import { BodyComponent } from './body/body.component';
 
 
 const routes: Routes = [
   {
     path: '',
-    component: MainComponent
+    component: BodyComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './main/header/header.component';
-import { FooterComponent } from './main/footer/footer.component';
-import { BodyComponent } from './main/body/body.component';
-import { MainComponent } from './main/main.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BodyComponent } from './body/body.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 // material
-import { AngularMaterialModule } from './models/angular-material.module';
+import { AngularMaterialModule } from './angular-material.module';
 
 // two way data binding
 import { FormsModule } from '@angular/forms';
@@ -25,7 +24,6 @@ import { HttpClientModule } from '@angular/common/http';
     HeaderComponent,
     FooterComponent,
     BodyComponent,
-    MainComponent
   ],
   imports: [
     BrowserModule,
